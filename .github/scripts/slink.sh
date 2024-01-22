15bf33c0caa802b5af1c3c24dd697335de271428
kamalabhishek <kamalabhishek.gunna@zemosolabs.com>
2024-01-22 20:00:37 +0530

    Update slink.sh

--- .github/scripts/slink.sh
+++ .github/scripts/slink.sh
@@ -5,6 +5,10 @@
 # Create the directory if it doesn't exist
 mkdir -p "content/product/"
 
+# Debugging: Print the content of files.json
+echo "Content of files.json:"
+cat files.json
+
 # Function to fetch files from a repository
 fetch_files() {
     local repo=$1
@@ -15,12 +19,10 @@ fetch_files() {
     echo "Fetching file from repo: $repo, key: $key, value: $value"
 
     # Use curl to download the file from the specified repo
-    # curl -LJO -u "$token":x-oauth-basic "https://raw.githubusercontent.com/kamalabhishek/$repo/main/$value"
-    curl -LJO -u "$token":x-oauth-basic "https://raw.githubusercontent.com/kamalabhishek/repo3/main/docs.md"
+    curl -LJO -u "$token":x-oauth-basic "https://raw.githubusercontent.com/kamalabhishek/$repo/main/$value"
 
     # Move the downloaded file to the specified directory in hbook
     mv "$value" "$key"
-    echo "File fetched and moved to: $key"
 }
 
 # Read the JSON file and fetch files
@@ -29,6 +31,9 @@ while IFS= read -r line; do
     key=$(echo "$line" | jq -r '.key')
     value=$(echo "$line" | jq -r '.value')
 
+    echo "Read from JSON: repo=$repo, key=$key, value=$value"
+
     fetch_files "$repo" "$key" "$value" "$TEST_TOKEN"
 done < files.json
+
 
