9273829095f1f78dbb0c28a7cb01fb2d06bc92d8
kamalabhishek <kamalabhishek.gunna@zemosolabs.com>
2024-01-23 11:27:30 +0530

    Update slink.sh

--- .github/scripts/slink.sh
+++ .github/scripts/slink.sh
@@ -1,35 +1,37 @@
 # ln -s https://raw.githubusercontent.com/kamalabhishek/repo3/main/docs.md content/product/docs.md
 
-#!/bin/bash
+# #!/bin/bash
 
-# Create the directory if it doesn't exist
-mkdir -p "content/product/"
+# # Create the directory if it doesn't exist
+# mkdir -p "content/product/"
 
-# Function to fetch files from a repository
-fetch_files() {
-    local repo=$1
-    local key=$2
-    local value=$3
-    local token=$4
+# # Function to fetch files from a repository
+# fetch_files() {
+#     local repo=$1
+#     local key=$2
+#     local value=$3
+#     local token=$4
 
-    echo "Fetching file from repo: $repo, key: $key, value: $value"
+#     echo "Fetching file from repo: $repo, key: $key, value: $value"
 
-    # Use curl to download the file from the specified repo
-    curl -LJO -u "$token":x-oauth-basic "https://raw.githubusercontent.com/kamalabhishek/$repo/main/$value"
-    ls
-    # Move the downloaded file to the specified directory in hbook
-    mv "$value" "$key"
-}
+#     # Use curl to download the file from the specified repo
+#     curl -LJO -u "$token":x-oauth-basic "https://raw.githubusercontent.com/kamalabhishek/$repo/main/$value"
+#     ls
+#     # Move the downloaded file to the specified directory in hbook
+#     mv "$value" "$key"
+# }
 
-# Read the JSON file and fetch files
-while IFS= read -r line; do
-    repo=$(echo "$line" | jq -r '.repo')
-    key=$(echo "$line" | jq -r '.key')
-    value=$(echo "$line" | jq -r '.value')
+# # Read the JSON file and fetch files
+# while IFS= read -r line; do
+#     repo=$(echo "$line" | jq -r '.repo')
+#     key=$(echo "$line" | jq -r '.key')
+#     value=$(echo "$line" | jq -r '.value')
 
-    echo "Read from JSON: repo=$repo, key=$key, value=$value"
+#     echo "Read from JSON: repo=$repo, key=$key, value=$value"
 
-    fetch_files "$repo" "$key" "$value" "$TEST_TOKEN"
-done < files.json
+#     fetch_files "$repo" "$key" "$value" "$TEST_TOKEN"
+# done < files.json
 
 
+
+curl -sL -H https://raw.githubusercontent.com/kamalabhishek/repo3/main/docs.md content/product/docs.md -o "content/product/"
